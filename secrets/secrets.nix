ed0b9e374fef0c37162721dfb90fe7bee8499e99
ecomex <skoch@sks-concept.de>
2024-11-13 13:36:21 +0100

    changes update for hosts

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,10 +2,14 @@ let
     # Systems
     n1x-d3skt0p = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID46gTXMD1xUO83BWBUWdNDctdc7r/RdbZ1a0m2Lg6kh";
 
+    systems = [n1x-d3skt0p];
+
     # Users
     ecomex = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJR86g7+g3l7w1pLd40J/NcStNRNDIOnOXFYl775ySUQ";
 
+    users = [ecomex];
+
 
 in {
-    "tailscale-key.age".publicKeys = [ n1x-d3skt0p ecomex ];
+    "tailscale-key.age".publicKeys = systems ++ users;
 }
