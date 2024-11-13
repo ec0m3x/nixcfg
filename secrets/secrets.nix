f2682b45478977edbccbcbc924b8a10ec1d56295
ecomex <skoch@sks-concept.de>
2024-11-13 08:44:01 +0100

    changes to secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,11 @@
 let
-    n1x-d3skt0p = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID2IkvIN8TZJjSRzb8k1jeJd+y5neI5zkoOF+QZWVo1S";
+    # Systems
+    n1x-d3skt0p = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID46gTXMD1xUO83BWBUWdNDctdc7r/RdbZ1a0m2Lg6kh";
+
+    # Users
+    ecomex = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJR86g7+g3l7w1pLd40J/NcStNRNDIOnOXFYl775ySUQ";
+
 
 in {
-    "ecomex-secrets.age".publicKeys = [ n1x-d3skt0p ];
-    "tailscale-key.age".publicKeys = [ n1x-d3skt0p ];
+    "tailscale-key.age".publicKeys = [ n1x-d3skt0p ecomex ];
 }
