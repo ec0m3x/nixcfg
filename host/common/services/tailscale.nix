80fb1ccefce8bbc60667d4d0a4afc23ef76b7959
ecomex <skoch@sks-concept.de>
2024-11-17 17:21:34 +0100

    changed ts key config

--- host/common/services/tailscale.nix
+++ host/common/services/tailscale.nix
@@ -1,8 +1,17 @@
-_: {
+{ config, self, ... }:{
+
   services.tailscale = {
     enable = true;
     useRoutingFeatures = "both";
+    authKeyFile = config.age.secrets.tailscalekey.path;
     openFirewall = true;
+  };
+
+  age.secrets.tailscalekey = {
+    file = "${self}/secrets/tailscalekey.age";
+    owner = "ecomex";
+    group = "users";
+    mode = "600";
   };
 
   networking.firewall.trustedInterfaces = [ "tailscale0" ];
