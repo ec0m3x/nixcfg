424a99ba79f262a9340ed406b9c184e94786b0a0
ecomex <skoch@sks-concept.de>
2024-11-17 19:26:40 +0100

    rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,7 @@
 let
     # Systems
     n1x-hd-1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOVRevT9Y1XKuS5+MhWEXL3i4kA/l7UsT380bY2U6Oib";
-    n1x-hs-1-vm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIbbIKshoEQniUrFXDGfVV2hK1sKvafpK7et96YBbvAS";
-    n1x-hs-1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINqzb52vHcbbv4uG9IJsDS18u4Lmbuo0p3e7rCdX/wPh";
+    n1x-hs-1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII95FnsCN1Kpk8TmkoV51LhRvZc3cFYGNUI3AF5tIaj9";
 
     # Users
     ecomex = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDqiPSANwQ8T9MTUTRZLX9ug0K5fRGwOYbnI0bMZ7gjC";
@@ -11,6 +10,6 @@ let
 
 
 in {
-    "n1x-hs-1-cloudflare.age".publicKeys = users ++ [n1x-hs-1] ++ [n1x-hs-1-vm];
-    "tailscalekey.age".publicKeys = users ++ [n1x-hd-1] ++ [n1x-hs-1] ++ [n1x-hs-1-vm];
+    "n1x-hs-1-cloudflare.age".publicKeys = users ++ [n1x-hs-1];
+    "tailscalekey.age".publicKeys = users ++ [n1x-hd-1] ++ [n1x-hs-1];
 }
