c226fa498d883f06428fc0f5350fc70c23d5940e
ecomex <skoch@sks-concept.de>
2024-11-18 16:33:39 +0100

    error fixed

--- home/common/users/ecomex/n1x-hs-1.nix
+++
@@ -1,9 +0,0 @@
-/* _: {
-  services.syncthing = {
-    enable = true;
-    extraOptions = [
-      "-gui-address=n1x-hs-1.taile8253.ts.net:8384"
-      "-home=/home/ecomex/.syncthing"
-    ];
-  };
-} */
