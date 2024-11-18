0066b44bbaa2403702469b99f44b9465c88417cb
ecomex <skoch@sks-concept.de>
2024-11-18 12:03:19 +0100

    syncthing

--- home/common/users/ecomex/n1x-hs-1.nix
+++ home/common/users/ecomex/n1x-hs-1.nix
@@ -1,3 +1,9 @@
 _: {
-
+  services.syncthing = {
+    enable = true;
+    extraOptions = [
+      "-gui-address=n1x-hs-1.taile8253.ts.net:8384"
+      "-home=/home/ecomex/data/.syncthing"
+    ];
+  };
 }
