a8e96179ee80b98eeb1124292e312a74a9286616
ecomex <skoch@sks-concept.de>
2024-11-22 07:46:54 +0100

    added searx key env var

--- host/common/services/searx.nix
+++ host/common/services/searx.nix
@@ -1,11 +1,20 @@
-{ pkgs, ... }: 
+{ pkgs, config, ... }: 
 {
   services.searx = {
     enable = true;
+    environmentFile = config.age.secrets.searx-key.path;
     settings = {
       server.port = 8080;
       server.bind_address = "0.0.0.0";
       server.base_url = "https://search.sks-concept.de";
+      server.secret_key = "@SEARX_SECRET_KEY@";
     };
+  };
+
+  age.secrets.searx-key = {
+    file = "${self}/secrets/searx-key.age";
+    owner = "ecomex";
+    group = "users";
+    mode = "600";
   };
 }
