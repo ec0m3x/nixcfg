8576c80922e3d6faeaa8bcc9bc9881df473b62b7
ecomex <skoch@ks-concept.de>
2024-12-23 20:45:50 +0100

    fixed disk layout n1x-hs-2

--- host/n1x-hs-2/disks.nix
+++ host/n1x-hs-2/disks.nix
@@ -1,81 +1,35 @@
 {
   lib,
-  disks ? [
-    "/dev/nvme0n1"
-  ],
   ...
 }:
-let
-  defaultBtrfsOpts = [
-    "defaults"
-    "compress=zstd:1"
-    "ssd"
-    "noatime"
-    "nodiratime"
-  ];
-in
 {
-
   disko.devices = {
     disk = {
-      # 1TB root/boot drive. Configured with:
-      # - A FAT32 ESP partition for systemd-boot
-      # - A LUKS container which containers multiple btrfs subvolumes for nixos install
-      nvme0n1 = {
+      nixos = {
         type = "disk";
-        device = builtins.elemAt disks 0;
+        device = "/dev/vda";
         content = {
           type = "gpt";
           partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
             ESP = {
-              label = "boot";
-              name = "ESP";
               size = "512M";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = [
-                  "defaults"
-                ];
               };
             };
-            luks = {
+            root = {
               size = "100%";
               content = {
-                type = "luks";
-                name = "cryptroot";
-                extraOpenArgs = [
-                  "--allow-discards"
-                ];
-                content = {
-                  type = "btrfs";
-                  # Override existing partition
-                  extraArgs = [ "-f" ];
-                  subvolumes = {
-                    "@" = {
-                      mountpoint = "/";
-                      mountOptions = defaultBtrfsOpts;
-                    };
-                    "@nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = defaultBtrfsOpts;
-                    };
-                    "@home" = {
-                      mountpoint = "/home";
-                      mountOptions = defaultBtrfsOpts;
-                    };
-                    "@var" = {
-                      mountpoint = "/var";
-                      mountOptions = defaultBtrfsOpts;
-                    };
-                    "@snapshots" = {
-                      mountpoint = "/.snapshots";
-                      mountOptions = defaultBtrfsOpts;
-                    };
-                  };
-                };
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
               };
             };
           };
